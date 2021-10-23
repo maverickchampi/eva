80f05c04aaa244c95a83c1cc4385ade8c9813370
Maverick <maverick.champi@gmail.com>
2021-10-22 20:36:13 -0500

    avance vistas

---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import IconoFb from "./../assets/icons/fb_logo.svg";
+import IconoGoogle from "./../assets/icons/google_logo.png";
+import ImagenLogin from "./../assets/images/ig_login.svg";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login__item">
+        <img src={ImagenLogin} alt="" className="login__item-img" />
+      </div>
+      <div className="login__item">
+        <img src="" alt="" className="login__item-img" />
+        <h2 className="titulo">Iniciar Sesión</h2>
+        <form className="form-contenedor">
+          <div className="grupo-input">
+            <i className="fas fa-user"></i>
+            <input type="text" name="correo" id="correo" value="madeliyricra" />
+          </div>
+          <div className="grupo-input">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              name="contrasenia"
+              id="contrasenia"
+              value="12345687"
+            />
+          </div>
+          <Link to="#" className="link-password">
+            ¿Te has olvidado tu contraseña?
+          </Link>
+          <button className="boton mb-1 mt-4">Ingresar</button>
+        </form>
+        <p className="link-registro">
+          ¿No tienes una cuenta?
+          <Link to="#" className="link__item">
+            Registrar
+          </Link>
+        </p>
+        <div className="api-contenedor">
+          <div className="api__item">
+            <img src={IconoGoogle} alt="icono de google" />
+          </div>
+          <div className="api__item">
+            <img src={IconoFb} alt="icono de facebook" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/configuracion.js
+++ src/routes/configuracion.js
@@ -1,5 +1,6 @@
 import Inicio from "../pages/Inicio";
 import Error404 from "../pages/Error404";
+import Login from "../pages/Login";
 
 export const routes = [
   {
@@ -7,6 +8,10 @@ export const routes = [
     exact: true,
     render: () => <Inicio />,
   },
-
+  {
+    path: "/login",
+    exact: true,
+    render: () => <Login />,
+  },
   { path: "", exact: false, render: () => <Error404 /> },
 ];
