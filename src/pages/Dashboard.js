9365afb25660473bbbe6d323c2fc2609853a1368
Maverick <maverick.champi@gmail.com>
2021-11-17 13:04:27 -0500

    animos

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,9 +7,15 @@ import { Chat } from "../components/atom/Chat";
 import { ESTADOS_ANIMOS } from "./../data/global";
 import { Line } from "react-chartjs-2";
 import Calendario from "../components/molecule/Calendario";
+import swal from "sweetalert";
+import { putData } from "../services/login";
 
 const Dashboard = () => {
+  const [video, setVideo] = useState(
+    "https://cdnl.iconscout.com/lottie/premium/preview-watermark/drooling-cat-3978275-3291095.mp4"
+  );
   const [animo, setAnimo] = useState([0, 0, 0, 0, 0, 0]);
+  const [fechaHoy, setFechaHoy] = useState({ animo: { animo: 0 } });
 
   const data = {
     labels: [
@@ -46,6 +52,7 @@ const Dashboard = () => {
   }, []);
 
   useEffect(() => {
+    console.log(user);
     let cantidad = [
       { valor: 0, cantidad: 0 },
       { valor: 0, cantidad: 0 },
@@ -58,6 +65,16 @@ const Dashboard = () => {
     if (user.calendarioAnimo.length > 0) {
       user.calendarioAnimo.forEach((element) => {
         if (element.animo !== 0) {
+          if (
+            new Date(element.fecha).toLocaleDateString() ==
+            new Date().toLocaleDateString()
+          ) {
+            setFechaHoy({
+              ...fechaHoy,
+              animo: { fecha: element.fecha, animo: element.animo },
+            });
+          }
+
           switch (new Date(element.fecha).getMonth()) {
             case 6:
               cantidad[0].valor += element.animo;
@@ -87,34 +104,96 @@ const Dashboard = () => {
         }
       });
 
-      console.log(cantidad);
+      // console.log(cantidad);
 
       setAnimo([
         cantidad[0].cantidad > 0
-          ? (cantidad[0].valor / cantidad[0].cantidad).toFixed(0)
+          ? Number((cantidad[0].valor / cantidad[0].cantidad).toFixed(0))
           : 0,
         cantidad[1].cantidad > 0
-          ? (cantidad[1].valor / cantidad[1].cantidad).toFixed(0)
+          ? Number((cantidad[1].valor / cantidad[1].cantidad).toFixed(0))
           : 0,
         cantidad[2].cantidad > 0
-          ? (cantidad[2].valor / cantidad[2].cantidad).toFixed(0)
+          ? Number((cantidad[2].valor / cantidad[2].cantidad).toFixed(0))
           : 0,
         cantidad[3].cantidad > 0
-          ? (cantidad[3].valor / cantidad[3].cantidad).toFixed(0)
+          ? Number((cantidad[3].valor / cantidad[3].cantidad).toFixed(0))
           : 0,
         cantidad[4].cantidad > 0
-          ? (cantidad[4].valor / cantidad[4].cantidad).toFixed(0)
+          ? Number((cantidad[4].valor / cantidad[4].cantidad).toFixed(0))
           : 0,
         cantidad[5].cantidad > 0
-          ? (cantidad[5].valor / cantidad[5].cantidad).toFixed(0)
+          ? Number((cantidad[5].valor / cantidad[5].cantidad).toFixed(0))
           : null,
       ]);
+
+      if (cantidad[4].cantidad > 0) {
+        if (
+          Number((cantidad[4].valor / cantidad[4].cantidad).toFixed(0)) === 1
+        ) {
+          setVideo(
+            "https://cdnl.iconscout.com/lottie/premium/preview-watermark/sad-cat-3978265-3291085.mp4"
+          );
+        } else {
+          setVideo(
+            "https://cdnl.iconscout.com/lottie/premium/preview-watermark/drooling-cat-3978275-3291095.mp4"
+          );
+        }
+      }
 
       setTimeout(() => {
         console.log(animo);
       }, 0);
     }
-  }, []);
+  }, [user]);
+
+  const mensaje = () => {
+    let name = user.nombre;
+    switch (animo[4]) {
+      case 3:
+        return `Hola ${name}, estos últimos días has estado genial, todo en base a tu esfuerzo, felicidades `;
+
+      case 2:
+        return `Hola ${name}, estos últimos días has estado emocionalmente regular, sigue así ánimos`;
+
+      case 1:
+        return `Hola ${name}, estos últimos días has estado fatal, porfavor te recomiendo seguir los consejos o hablar con EVA `;
+    }
+  };
+  // console.log(fechaHoy);
+  const actualizaEstado = (value) => {
+    const _user = user;
+
+    const position = _user.calendarioAnimo.findIndex(
+      (e) =>
+        new Date(e.fecha).toLocaleDateString() ==
+        new Date().toLocaleDateString()
+    );
+
+    if (position === -1) {
+      _user.calendarioAnimo.push({
+        fecha: new Date(),
+        animo: Number(value),
+      });
+    } else {
+      _user.calendarioAnimo[position].animo = Number(value);
+    }
+
+    setUser({});
+    setUser({ ..._user });
+
+    setTimeout(() => {
+      sessionStorage.setItem(btoa("user"), btoa(JSON.stringify({ ..._user })));
+    }, 0);
+
+    putData(JSON.stringify(_user))
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) =>
+        swal("Opps!", "Error al actualizar, intentelo nuevamente", "error")
+      );
+  };
 
   return (
     <div className="dashboard">
@@ -124,19 +203,52 @@ const Dashboard = () => {
           <div className=" card card-resumen">
             <div className="content-box">
               <div>
-                <img src={emojiResumen} />
+                <img
+                  src={user?.foto}
+                  alt={user?.nombre}
+                  style={{ width: "90px", borderRadius: "50%" }}
+                />
+                <video
+                  className="video"
+                  src={video}
+                  autoplay="autoplay"
+                  muted="muted"
+                  loop="loop"
+                  playsinline=""
+                  type="video/mp4"
+                ></video>
               </div>
               <div>
-                <p>En los ultimos dias John ha estado muy feliz ¡Sigue asi!</p>
+                <p>{mensaje()}</p>
               </div>
             </div>
             <div className="content-box-2">
               <p>¿Cómo estás hoy?</p>
-              <select>
-                <option value="0">Seleccione</option>
-                <option value="3">Bien</option>
-                <option value="2">Normal</option>
-                <option value="1">Mal</option>
+              <select onChange={(e) => actualizaEstado(e.target.value)}>
+                <option
+                  value="0"
+                  selected={fechaHoy?.animo?.animo == 0 ? true : false}
+                >
+                  Seleccione
+                </option>
+                <option
+                  value="3"
+                  selected={fechaHoy?.animo?.animo == 3 ? true : false}
+                >
+                  Bien
+                </option>
+                <option
+                  value="2"
+                  selected={fechaHoy?.animo?.animo == 2 ? true : false}
+                >
+                  Normal
+                </option>
+                <option
+                  value="1"
+                  selected={fechaHoy?.animo?.animo == 1 ? true : false}
+                >
+                  Mal
+                </option>
               </select>
             </div>
           </div>
