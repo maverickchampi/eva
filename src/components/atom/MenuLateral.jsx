74239eb2bb4389d0061a4ba3ffff15474a234f3a
Maverick <maverick.champi@gmail.com>
2021-11-19 19:52:12 -0500

    wsp integrado

--- src/components/atom/MenuLateral.jsx
+++ src/components/atom/MenuLateral.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/eva.png";
+import WSPLink from "./WSPLink";
 
 const MenuLateral = ({ link = 0 }) => {
   return (
-    <header className="menuLateral">
-      <Link to="/dashboard" className="link">
-        <img src={logo} alt="logo" />
-      </Link>
-      <nav>
-        <Link to="/dashboard" className={`link ${link === 0 ? "active" : ""}`}>
-          <i className="fas fa-home"></i>
+    <>
+      <header className="menuLateral">
+        <Link to="/dashboard" className="link">
+          <img src={logo} alt="logo" />
         </Link>
-        <Link to="/perfil" className={`link ${link === 1 ? "active" : ""}`}>
-          <i className="fas fa-user"></i>
+        <nav>
+          <Link
+            to="/dashboard"
+            className={`link ${link === 0 ? "active" : ""}`}
+          >
+            <i className="fas fa-home"></i>
+          </Link>
+          <Link to="/perfil" className={`link ${link === 1 ? "active" : ""}`}>
+            <i className="fas fa-user"></i>
+          </Link>
+          <Link to="/plus" className={`link ${link === 2 ? "active" : ""}`}>
+            <i className="fas fa-plus"></i>
+          </Link>
+          <Link to="/box" className={`link ${link === 3 ? "active" : ""}`}>
+            <i className="fas fa-box"></i>
+          </Link>
+        </nav>
+        <Link to="" className="link">
+          <i className="fas fa-power-off"></i>
         </Link>
-        <Link to="/plus" className={`link ${link === 2 ? "active" : ""}`}>
-          <i className="fas fa-plus"></i>
-        </Link>
-        <Link to="/box" className={`link ${link === 3 ? "active" : ""}`}>
-          <i className="fas fa-box"></i>
-        </Link>
-      </nav>
-      <Link to="" className="link">
-        <i className="fas fa-power-off"></i>
-      </Link>
-    </header>
+      </header>
+      <WSPLink />
+    </>
   );
 };
 
---
+++ src/components/atom/WSPLink.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const WSPLink = () => {
+  return (
+    <div className="wsp_logo">
+      <a
+        href="https://api.whatsapp.com/send?phone=14155238886&text=Hola%20EVA"
+        target="_blank"
+      >
+        <i class="fas fa-phone-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default WSPLink;
