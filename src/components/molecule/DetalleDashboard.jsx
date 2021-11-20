156783de779ffa104ba37fc62ef1a7952a9e784c
Maverick <maverick.champi@gmail.com>
2021-11-19 22:49:41 -0500

    emojis y calendario

--- src/components/molecule/DetalleDashboard.jsx
+++ src/components/molecule/DetalleDashboard.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const DetalleDashboard = () => {
-  return <div class="detalle-dashboard"></div>;
+  const [events, setEvents] = useState([
+    { id: 1, start: "2021-11-18", title: "😀" },
+    { id: 2, start: "2021-11-17", title: "😡" },
+    { id: 3, start: "2021-11-16", title: "😢" },
+    { id: 4, start: "2021-11-15", title: "😲" },
+    { id: 5, start: "2021-11-14", title: "🥰" },
+  ]);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div class="detalle-dashboard">
+      <div className="calendario">
+        <FullCalendar
+          events={events}
+          initialDate="2021-11-30"
+          initialView="dayGridMonth"
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "",
+          }}
+        />
+      </div>
+      <div className="recomendaciones">
+        <div>recomendaciones</div>
+      </div>
+    </div>
+  );
 };
 
 export default DetalleDashboard;
