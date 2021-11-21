66f282dc5d6726675981b31db174382ad8b8c014
madeliyricra <madeliyricra@gmail.com>
2021-11-21 15:42:25 -0500

    form de registro y login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Background from "../components/atom/Background";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login">
+      <Background />
+      <div className="login-content">
+        <div className="login__item"></div>
+        <div className="login__item">
+          <h1>Login</h1>
+          <form>
+            <input type="email" name="correo" />
+            <input type="text" name="contrasenia" />
+            <button>Ingresar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Background from "../components/atom/Background";
 
 const Registro = () => {
-  return <div></div>;
+  return (
+    <div className="registro">
+      <Background clase="back-registro" />
+      <div className="registro-content">
+        <div className="registro__item"></div>
+        <div className="registro__item">
+          <h1>Registrarse</h1>
+          <form>
+            <input type="text" placeholder="Nombre" name="nombre" />
+            <input
+              type="text"
+              name="apellido_pa"
+              placeholder="Apellido paterno"
+            />
+            <input
+              type="text"
+              name="apellido_ma"
+              placeholder="Apellido maaterno"
+            />
+            <input type="date" name="fecha_nac" />
+            <input type="email" name="correo" placeholder="Correo" />
+            <input type="password" name="contraseni" placeholder="Contraseña" />
+            <button>Registrarse</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Registro;
