db84bd533b8692e80f0116adaa5840b1e108783c
madeliyricra <madeliyricra@gmail.com>
2021-11-21 17:42:09 -0500

    avance de diseño de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Background from "../components/atom/Background";
 
 const Login = () => {
@@ -6,14 +7,41 @@ const Login = () => {
     <div className="login">
       <Background />
       <div className="login-content">
-        <div className="login__item"></div>
         <div className="login__item">
-          <h1>Login</h1>
-          <form>
-            <input type="email" name="correo" />
-            <input type="text" name="contrasenia" />
-            <button>Ingresar</button>
-          </form>
+          <h2></h2>
+        </div>
+        <div className="login__item">
+          <div className="login-formulario">
+            <h1 className="titulo">
+              <span>Iniciar Sesión</span>
+            </h1>
+            <form>
+              <div className="form-group">
+                <label htmlFor="correo">Email</label>
+                <input type="email" name="correo" placeholder="Correo" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="contrasenia">Contraseña</label>
+                <input
+                  type="text"
+                  name="contrasenia"
+                  placeholder="Contraseña"
+                />
+              </div>
+              <Link to="#">¿Has olvidado tu contraseña?</Link>
+              <button>Ingresar</button>
+            </form>
+            <div className="login-api">
+              <div className="api__item google">
+                <i class="fab fa-google"></i>
+                <p>Ingresar con Google</p>
+              </div>
+              <div className="api__item facebook">
+                <i class="fab fa-facebook"></i>
+                <p>Ingresar con Facebook</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
