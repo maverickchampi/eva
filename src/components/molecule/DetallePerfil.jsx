75261f83290e2a9528a7653fa2ecf1ecfa1dd07e
madeliyricra <madeliyricra@gmail.com>
2021-11-25 00:25:30 -0500

    avance de vista recomendaciones

--- src/components/molecule/DetallePerfil.jsx
+++ src/components/molecule/DetallePerfil.jsx
@@ -3,8 +3,20 @@ import React from "react";
 const DetallePerfil = () => {
   return (
     <div className="detalle-perfil">
-      <div className="pregunta">question</div>
-      <div className="recomendaciones">recomendacion</div>
+      <div className="pregunta">
+        <h3>Pregunta del día</h3>
+        <hr />
+      </div>
+      <div className="recomendaciones">
+        <h3>Recomendaciones</h3>
+        <hr />
+        <div className="recomendacion-content">
+          <div className="recomendacion__item">1</div>
+          <div className="recomendacion__item">2</div>
+          <div className="recomendacion__item">3</div>
+        </div>
+        <hr />
+      </div>
     </div>
   );
 };
