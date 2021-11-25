6412560f15a6a9ed190aeab8d8289af24fdfe25c
Maverick <maverick.champi@gmail.com>
2021-11-25 00:14:35 -0500

    divs

--- src/components/molecule/DetallePerfil.jsx
+++ src/components/molecule/DetallePerfil.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const DetallePerfil = () => {
-  return <div className="detalle-perfil">aaaa</div>;
+  return (
+    <div className="detalle-perfil">
+      <div className="pregunta">question</div>
+      <div className="recomendaciones">recomendacion</div>
+    </div>
+  );
 };
 
 export default DetallePerfil;
