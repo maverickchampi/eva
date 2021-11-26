14ac406158dc8e22499619d160c563cd7974caa9
madeliyricra <madeliyricra@gmail.com>
2021-11-25 20:53:45 -0500

    vista detalle de perfil

--- src/components/molecule/DetallePerfil.jsx
+++ src/components/molecule/DetallePerfil.jsx
@@ -1,21 +1,41 @@
 import React from "react";
+import { PRUEBAS } from "../../constants/global";
 
 const DetallePerfil = () => {
   return (
     <div className="detalle-perfil">
       <div className="pregunta">
-        <h3>Pregunta del día</h3>
-        <hr />
+        {/* <h3>Pregunta del día</h3> */}
+        <div className="pregunta-content">
+          <h5>¿Como te sientes hoy?</h5>
+          <ul className="animo-content">
+            <li className="on">Bien</li>
+            <li>Regular</li>
+            <li>Mal</li>
+            <li>No lo sé</li>
+          </ul>
+          <textarea></textarea>
+          <div className="botones">
+            <button>Guardar</button>
+          </div>
+        </div>
       </div>
       <div className="recomendaciones">
         <h3>Recomendaciones</h3>
-        <hr />
         <div className="recomendacion-content">
-          <div className="recomendacion__item">1</div>
-          <div className="recomendacion__item">2</div>
-          <div className="recomendacion__item">3</div>
+          {PRUEBAS.map((item, index) => {
+            return (
+              <div className="recomendacion__item" key={index}>
+                <span>{index + 1}</span>
+                <img src={item.imagen} alt={item.titulo} />
+                <div className="recomendacion-descripcion">
+                  <h5>{item.titulo}</h5>
+                  <p>{item.descripcion}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <hr />
       </div>
     </div>
   );
