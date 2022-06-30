3bbfe70dc8883e754e8fe59b5d5499efdbae2ae7
madeliyricra <madeliyricra@gmail.com>
2022-06-29 20:27:42 -0500

    version 2.0

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Dashboard } from "./Dashboard"
+export { default as Gallery } from "./Gallery"
+export { default as Landing } from "./Landing"
+export { default as Login } from "./Login"
+export { default as Meet } from "./Meet"
+export { default as Profile } from "./Profile"
+export { default as Register } from "./Register"
---
+++ src/routes/index.jsx
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import { LINK_DASHBOARD } from "../utils/link";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path={LINK_DASHBOARD} component={Dashboard} />
+        <Redirect to={LINK_DASHBOARD} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
